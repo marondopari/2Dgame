23a9ba93ac7f683e9794b6f313265433ab4fa40d
marondopari <mikaexo930@gmail.com>
2022-04-15 15:54:12 +0900

    クモの移動範囲を制限するスクリプトを追加

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float speed = 5f;        //プレイヤーの移動速度
+    public float xMovebleRange = 6.5f;       //プレイヤーのX方向移動範囲
+    public float yMovebleRange = 3.5f;       //プレイヤーのY方向移動範囲
 
     void Start()
     {
@@ -13,5 +15,8 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         transform.position += new Vector3(Input.GetAxisRaw("Horizontal") * speed * Time.deltaTime,Input.GetAxisRaw("Vertical") * speed * Time.deltaTime);     //プレイヤーを上下左右動かす処理
+
+        transform.position = new Vector3(Mathf.Clamp(transform.position.x, -xMovebleRange, xMovebleRange), Mathf.Clamp(transform.position.y, -yMovebleRange, yMovebleRange));     //移動範囲を制限する処理
+
     }
 }
